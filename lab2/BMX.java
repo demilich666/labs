271bb06a649626d2c38ecb5f2e92bfa0f957110b
demilich666 <smycorpse@gmail.com>
2026-05-25 15:52:40 +0300

    Update BMX.java

--- lab2/BMX.java
+++ lab2/BMX.java
@@ -6,5 +6,19 @@ public class BMX extends Bicycle{
    public BMX(String color, String model, int wheelsCount, int speed, String rotating, String sound) {
        super(color, model, wheelsCount, speed, rotating, sound);
    }
+  
+ @Override
+   public void riding() {
+       System.out.println("BMX делает трюки");
+   }
+
+
+   @Override
+   public void makeSound() {
+       System.out.println("BMX не имеет звонка");
+
+
+   }
 }
+
 
