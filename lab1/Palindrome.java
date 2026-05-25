ffd86fb74a293b1d6507b280c2cb85ec4e651ec6
demilich666 <smycorpse@gmail.com>
2026-05-25 15:38:12 +0300

    Create Palindrome.java

---
+++ lab1/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            boolean result = isPalindrome(s);
+            System.out.println(s + " " +result);
+        }
+    }
+    public static String reverseString(String s) {
+        String reversed = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            reversed += s.charAt(s.length() - 1 - i);
+        }
+        return reversed;
+    }
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+}
