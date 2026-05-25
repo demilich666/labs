7faf8558d9f26dc1cb62d809167a0b4274a4d351
demilich666 <smycorpse@gmail.com>
2026-05-25 15:51:53 +0300

    Create BMX.java

---
+++ lab2/BMX.java
@@ -0,0 +1,10 @@
+package org.example.lab2;
+
+public class BMX extends Bicycle{
+
+  
+   public BMX(String color, String model, int wheelsCount, int speed, String rotating, String sound) {
+       super(color, model, wheelsCount, speed, rotating, sound);
+   }
+}
+
